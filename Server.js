93c4922a589546f6c244aef59817c51853376145
Idyege Nathaniel <idyegenathaniel@gmail.com>
2024-10-20 13:18:48 +0100

    update

--- Server.js
+++ Server.js
@@ -34,6 +34,8 @@ app.use(notFound);
 app.use(errorHandler);
 
 
-app.listen(port, () => console.log(`This server is running on port ${port}`));
+if (process.env.NODE_ENV !== "production") {
+    app.listen(port, () => console.log(`This server is running on port ${port}`));
+}
 
 export default app;
